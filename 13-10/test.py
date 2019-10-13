884276cae11fbdc26866fa401ec7ed3b0c274539
Danya <danyanedostup@gmail.com>
2019-10-13 09:04:02 +0000

    lesson 13.10

---
+++ 13-10/test.py
@@ -0,0 +1,29 @@
+# # числа в десятичной системе счисления
+# dec1 = 8
+# dec2 = 42
+# dec3 = -3
+# dec4 = 25802836572356723058203845293402834028304820938402834023580235777082489436236
+# dec5 = 40989786754321
+
+# print(dec1)
+# print(dec2)
+# print(dec3)
+# print(dec4)
+# print(dec5)
+# print()
+
+
+hex1 = 0x9
+hex2 = 0xA
+hex3 = 0xFF
+hex4 = 0x3de
+hex5 = 0xdd3
+
+print(hex1)
+print(hex2)
+print(hex3)
+print(hex4)
+print(hex5)
+print()
+
+
